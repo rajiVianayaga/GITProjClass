6446cb2e3218946a3599776e84b351a5e75655ff
semmalaselvi <malarveena.2004@gmail.com>
2021-10-08 12:21:39 +0530

    username funtionality

---
+++ src/test/java/org/git/FirstGit.java
@@ -0,0 +1,15 @@
+package org.git;
+
+public class FirstGit {
+	
+	public void username()
+	{
+		System.out.println("username");
+	}
+	public static void main(String[] args)
+	{
+		FirstGit f = new FirstGit();
+		f.username();
+	}
+
+}
